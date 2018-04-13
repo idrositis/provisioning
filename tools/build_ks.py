1022ee0190d8c2ce792b50214c9301d3968e51e8
Ioannis Drositis <jdros_el@yahoo.com>
2018-04-13 14:28:01 +0100

    Fix error message library error

--- tools/build_ks.py
+++ tools/build_ks.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 # Script to render kickstart template files, using Cheetah
 #
+from __future__ import print_function  # msg_to_stderr, warning, error, debug, info
+import sys                             # msg_to_stderr
+
 import argparse
 from argparse import RawTextHelpFormatter
+
+# Functions to print warning & debug MSGs
+def warning_msg(*objs):
+  print("WARN:", *objs, file=sys.stderr)
+
+def error_msg(*objs):
+  print("ERROR:", *objs, file=sys.stdout)
+
+def debug_msg(*objs):
+  print("DEBUG:", *objs, file=sys.stderr)
+
+def info_msg(*objs):
+  print("INFO:", *objs, file=sys.stderr)
 
 try:
   from Cheetah.Template import Template
 except ImportError:
-  error_msg("No Cheetah module detected!")
+  error_msg("No Cheetah module detected! How about `pip install Cheetah`?")
   exit(10)
 
 import os
@@ -28,9 +44,6 @@ def main():
                       help='File with function definitions (default: "{}")'.format(DEFAULT_FUNCTIONS_FILE))
   parser.add_argument('-v', '--variable_file', type=str,
                       help="Variable file")
-
-  parser.add_argument('-V', '--verbose', action='store_true',
-                      help='Increase output verbosity')
 
   # Read arguments
   args = parser.parse_args()
