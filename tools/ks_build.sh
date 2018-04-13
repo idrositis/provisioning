1d16fe14a82be1db3c79e180b81f3edca7cfb5a7
Ioannis Drositis <jdros_el@yahoo.com>
2018-04-13 15:53:03 +0100

    List kickstart template files in ks_build.sh; improve error-checking

--- tools/ks_build.sh
+++ tools/ks_build.sh
@@ -3,22 +3,49 @@
 
 # cheetah must be reachable
 which cheetah >/dev/null || {
-  echo "cheetah not found! Install cheetah (e.g. 'pip install cheetah') and retry..."
+  echo "[ERROR] cheetah not found!" >&2
+  echo "[ERROR] Install cheetah (eg. 'pip install cheetah') and retry..." >&2
   exit 1
+}
+
+function list_possible_tmpl_files()
+{
+  ls -1 cen*.tmpl 1>/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo
+    echo "Available templates:"
+    ls -1 cen*.tmpl | sed 's/^/ - /g'
+  fi
 }
 
 # Argument check
 if [ -z "$1" ]; then
-  echo "Script to build kickstart files out of a template using cheetah"
-  echo "Usage: ${0##*/} <kickstart_file.tmpl>"
-  echo " e.g.: ${0##*/} centos6.tmpl"
+  echo "Script to build kickstart files for RHEL/CentOS unattended installations"
+  echo "Machine configuration is taken from the 'variables' file"
+  echo
+  echo "Usage: ${0##*/} <kickstart_template_file.tmpl>"
+  echo "  eg.: ${0##*/} centos6[.tmpl]"
+  list_possible_tmpl_files
+  echo
   exit 0
+fi
+
+# File with variables should be present
+if [ ! -r "variables" ]; then
+  echo "[ERROR] File 'variables' is missing!"
+  echo "[ERROR] Either create a new one, or copy and modify one of the existing ones"
+  exit 1
 fi
 
 filename="$1"
 if [ ! -r "$filename" ]; then
-  echo "[ERROR] Cannot access file '$filename'!" >&2
-  exit 2
+  filenameTmpl="${filename}.tmpl"
+  if [ ! -r "$filenameTmpl" ]; then
+    echo "[ERROR] Cannot access file '$filename[.tmpl]!" >&2
+    exit 2
+  else
+    filename="$filenameTmpl"
+  fi
 fi
 
 # Main
